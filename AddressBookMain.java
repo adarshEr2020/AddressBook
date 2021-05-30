bff7b9ed8792ff986c647aa77d4be5813c7b62f4
adarshEr2020 <adarshprajapati31@gmail.com>
2021-05-30 10:07:37 +0530

    upadating details in address book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -78,7 +78,7 @@ class AddressBook {
 
 // here is main class
 public class AddressBookMain {
-	public static void main(String args[]) {
+public static void main(String[] args) {
 
 		System.out.println("Welcome To Address Book System");
 		Scanner sc = new Scanner(System.in);
@@ -121,13 +121,52 @@ public class AddressBookMain {
 
 				add[count] = new AddressBook(firstName, lastName, city, state, pin, mobileNo, email); 
 				count++;
-				break;
+				break; 
 				}
 			System.out.println("Details added successfully");
 
+			// here updating details in address book
+			case 2: System.out.println("Update Details");
+			System.out.println("Enter first name which you want to update");
+			firstName = sc.next();
+
+			for(int i = 0; i < count; i++) {
+				if(add[i] != null && add[i].getFirstName()==firstName) {
+
+					System.out.println("Enter first name and last name");
+					firstName = sc.next();
+					add[i].setFirstName(firstName);
+
+					lastName = sc.next();
+					add[i].setLastName(lastName);
+
+					System.out.println("Enter city");
+					city = sc.next();
+					add[i].setCity(city);
+
+					System.out.println("Enter state");
+					state = sc.next();
+					add[i].setState(state);
+
+					System.out.println("Enter pin");
+					pin = sc.next();
+					add[i].setPin(pin);
+
+					System.out.println("Enter Mobile number");
+					mobileNo = sc.next();
+					add[i].setMobileNo(mobileNo);
+
+					System.out.println("Enter Email Id");
+					email = sc.next();
+					add[i].setEmail(email);
+					break; 
+				}
+			}
+			System.out.println("Details updated successfully");
 			}
 		}
 
 	}
 
 }
+
